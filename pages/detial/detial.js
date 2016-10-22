5c510115e9db0483776e2bb54e45a5e5b0563fde
blue <bluedai180@gmail.com>
2016-10-22 17:31:18 +0800

    Add navigate to detial page

--- pages/detial/detial.js
+++ pages/detial/detial.js
@@ -1,29 +1,24 @@
 Page({
   data:{
     // text:"这是一个页面"
-    list:[]
-  },
-  tapName: function(event) {
-    console.log(event.currentTarget.id)
-    wx.navigateTo({
-      url:'../detial/detial?id=' + event.currentTarget.id,
-      id:event.currentTarget.id
-    })
-    
+    id:0,
+    message:''
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    console.log(options.id+"detial")
     var that = this;
-    const healthUri = 'http://www.tngou.net/api/' + options.type + '?id=3&rows=10'
+    this.setData({id:options.id+"detial"})
+    const detialUri = 'http://www.tngou.net/api/' + 'info/show?id=' + options.id
     wx.request({
-        url: 'http://www.tngou.net/api/info/list?id=3&rows=10',
+        url: detialUri,
         data: {},
         header: {
             'Content-Type': 'application/json'
         },
         success: function(res) {
-            that.setData({list: res.data.tngou})
-            console.log(res.data.tngou[0])
+            console.log(res.data.message)
+            that.setData({message:res.data.message})
             // var date = new Date(1477108880000);
             // var Y,M,D,h,m,s;
             // Y = date.getFullYear() + '-';
