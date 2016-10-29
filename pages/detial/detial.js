a326656ee501c22e669388796de513ce43599419
blue <bluedai180@gmail.com>
2016-10-29 17:45:30 +0800

    Add Search page

--- pages/detial/detial.js
+++ pages/detial/detial.js
@@ -1,46 +1,45 @@
 Page({
   data:{
-    // text:"这是一个页面"
     id:0,
     message:''
   },
   onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-    console.log(options.id+"detial")
     var that = this;
-    this.setData({id:options.id+"detial"})
-    const detialUri = 'http://www.tngou.net/api/' + 'info/show?id=' + options.id
-    wx.request({
-        url: detialUri,
-        data: {},
-        header: {
-            'Content-Type': 'application/json'
-        },
-        success: function(res) {
-            console.log(res.data.message)
-            that.setData({message:res.data.message})
-            // var date = new Date(1477108880000);
-            // var Y,M,D,h,m,s;
-            // Y = date.getFullYear() + '-';
-            // M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-            // D = date.getDate() + ' ';
-            // h = date.getHours() + ':';
-            // m = date.getMinutes() + ':';
-            // s = date.getSeconds();
-            // console.log(Y+M+D+h+m+s);
-        }
-    })
+    if(options.id != null) {
+      this.setData({id:options.id+"detial"})
+      const detialUri = 'http://www.tngou.net/api/' + 'info/show?id=' + options.id
+      wx.request({
+          url: detialUri,
+          data: {},
+          header: {
+              'Content-Type': 'application/json'
+          },
+          success: function(res) {
+              console.log(res.data.message)
+              that.setData({message:res.data.message})
+              // var date = new Date(1477108880000);
+              // var Y,M,D,h,m,s;
+              // Y = date.getFullYear() + '-';
+              // M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+              // D = date.getDate() + ' ';
+              // h = date.getHours() + ':';
+              // m = date.getMinutes() + ':';
+              // s = date.getSeconds();
+              // console.log(Y+M+D+h+m+s);
+          }
+      })
+    }
+    if(options.info != null) {
+      that.setData({message:options.info})
+    }
   },
   onReady:function(){
-    // 页面渲染完成
   },
   onShow:function(){
-    // 页面显示
   },
   onHide:function(){
-    // 页面隐藏
   },
   onUnload:function(){
-    // 页面关闭
+
   }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,20 +1,57 @@
 Page({
   data:{
-    // text:"这是一个页面"
+    inputValue: '',
+    chooseValue: '',
+    info: '',
+    items: [
+      {name: 'cook', value: '菜谱'},
+      {name: 'disease', value: '病症'},
+      {name: 'drug', value: '药品'},
+    ]
+  },
+  bindKeyInput: function(e) {
+    this.inputValue = e.detail.value
+  },
+  startSearch: function(e) {
+    var that = this;
+    console.log(this.inputValue + this.chooseValue)
+
+    wx.request({
+        url: 'http://www.tngou.net/api/' + that.chooseValue + '/name?name=' + that.inputValue,
+        data: {},
+        header: {
+            'Content-Type': 'application/json'
+        },
+        success: function(res) {
+          if (that.chooseValue == 'cook') {
+              wx.navigateTo({
+                  url:'../detial/detial?info=' + res.data.tngou[0].message,
+              })
+          } else {
+              wx.navigateTo({
+                  url:'../detial/detial?info=' + res.data.message,
+              })
+          }
+          
+        }
+    })
+    
+    console.log(this.info+'$$$$$$$$$$')
+
+
+  },
+  radioChange: function(e) {
+    this.chooseValue = e.detail.value
   },
   onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+    
   },
   onReady:function(){
-    // 页面渲染完成
   },
   onShow:function(){
-    // 页面显示
   },
   onHide:function(){
-    // 页面隐藏
   },
   onUnload:function(){
-    // 页面关闭
   }
 })
